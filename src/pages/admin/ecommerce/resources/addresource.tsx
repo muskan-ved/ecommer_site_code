d30a962b8d1ff19c0c3100fd4b08c77a7e71feaa
shubham jaiswal <govind.mangoitsolutions@gmail.com>
2023-07-21 20:21:00 +0530

    changes

--- src/pages/admin/ecommerce/resources/addresource.tsx
+++ src/pages/admin/ecommerce/resources/addresource.tsx
@@ -1,22 +1,51 @@
 import React, { useState } from 'react';
 import Breadcrumb from '../../../../common/components/breadcrumbs';
 import { addresourceBreadcrums } from '../../../../common/components/breadcrumbs/breadcrumbsData';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { resourceValidations } from '../../../../formvalidations/resourceformvalidation';
 import { ErrorShowing } from '../../../../common/components/errorshowingcmp/errorshowingcmp';
 import { Link } from 'react-router-dom';
+import { RiCloseCircleFill } from 'react-icons/ri';
 
 const AddResources = () => {
+    const [resourceimage, setresourceimage] = useState('')
+    const [resourcesubimage, setresourcesubimage] = useState('')
     const {
         register,
         handleSubmit,
+        setValue,
         formState: { errors },
     } = useForm<any>({
         resolver: yupResolver(resourceValidations),
     });
+
+    const handleChange = (e: any) => {
+        const file = e.target.files[0];
+        const multiplefile = e.target.files;
+        const reader = new FileReader();
+        if (e.target.name === "resourceimg" && file) {
+            setValue("resourceimg", file);
+            reader.onload = (e: any) => {
+                setresourceimage(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        } else if (e.target.name === "resourcesubimg" && multiplefile) {
+            setValue("resourcesubimg", file);
+            reader.onload = (e: any) => {
+                setresourcesubimage(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        };
+    };
+    const resetresourceimage = () => {
+        setresourceimage("");
+    }
+    const resetresourcesubimage = () => {
+        setresourcesubimage("");
+    }
+
+
     const onSubmit = (data: any) => {
         alert(JSON.stringify(data))
     }
@@ -81,34 +110,49 @@ const AddResources = () => {
                                     <label className="block text-sm">
                                         Resource Main Image
                                     </label>
-                                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 mt-1.5">
-                                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                            </svg>
-                                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                                        </div>
-                                        <input id="dropzone-file" type="file" className="hidden" />
-                                    </label>
+                                    {resourceimage ?
+                                        <div className="flex flex-col items-center justify-center w-full h-32  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 mt-1.5">
+                                            <div className="img_wrp ">
+                                                <img className="object-cover h-24 w-24" src={resourceimage} />
+                                                <div onClick={resetresourceimage} className="absolute top-0 right-0 text-red-600	 "><RiCloseCircleFill /></div>
+                                            </div></div>
+                                        : (
+                                            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 mt-1.5">
+                                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                                    </svg>
+                                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                                </div>
+                                                <input id="dropzone-file" {...register("resourceimg")} onChange={handleChange} type="file" className="hidden" />
+                                            </label>)}
                                 </div>
                                 <div className="md:w-1/2 px-3">
                                     <label className="block text-sm">
                                         Resource Sub Images
                                     </label>
-                                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 mt-1.5">
-                                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                            </svg>
-                                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                                        </div>
-                                        <input id="dropzone-file" type="file" className="hidden" />
-                                    </label>
+                                    {resourcesubimage ?
+                                        <div className="flex flex-col items-center justify-center w-full h-32  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 mt-1.5">
+                                            <div className="img_wrp ">
+                                                <img className="object-cover h-24 w-24" src={resourcesubimage} />
+                                                <div onClick={resetresourcesubimage} className="absolute top-0 right-0 text-red-600	 "><RiCloseCircleFill /></div>
+                                            </div></div>
+                                        : (
+                                            <label htmlFor="dropzone-file1" className="flex flex-col items-center justify-center w-full h-32  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 mt-1.5">
+                                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                                    </svg>
+                                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                                </div>
+                                                <input id="dropzone-file1"  {...register("resourcesubimg")} onChange={handleChange} type="file" multiple className="hidden" />
+                                            </label>
+                                        )}
                                 </div>
                             </div>
-                            <div className="-mx-3 md:flex mt-5">
+                            <div className="-mx-3 md:flex mt-8">
                                 <div className="md:w-full px-3">
                                     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800  focus:outline-none font-medium rounded-md text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
                                     <Link to={"/admin/ecommerce/resources"}><button type="button" className=" ml-5 text-white bg-red-700 hover:bg-red-800  focus:outline-none  font-medium rounded-md text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">CANCEL</button></Link>
--- src/pages/admin/users/adduser.tsx
+++ src/pages/admin/users/adduser.tsx
@@ -150,18 +150,20 @@ const AddUser = () => {
                                         Profile Pic
                                     </label>
                                     <div className="flex items-center justify-center w-full mt-1.5">
-                                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-12 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                            {profilepic ? <div className="img_wrp ">
-                                                <img className="object-cover h-8 w-15" src={profilepic} />
-                                                <div onClick={resetprofilepic} className="absolute top-0 right-0 text-red-600	 "><RiCloseCircleFill /></div>
-                                            </div> : <div className="flex flex-col items-center justify-center">
-                                                <svg className="w-8 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                                </svg>
-                                                <p className="text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> (SVG, PNG, JPG, JPEG or GIF)</p>
-                                            </div>}
-                                            <input id="dropzone-file" type="file" accept=".gif,.jpg,.jpeg,.png,.svg"  {...register("profilepic")} onChange={handleimagechange} className="hidden" />
-                                        </label>
+                                        {profilepic ?
+                                            <div className="flex flex-col items-center justify-center w-full h-12 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                                <div className="img_wrp ">
+                                                    <img className="object-cover h-8 w-15" src={profilepic} />
+                                                    <div onClick={resetprofilepic} className="absolute top-0 right-0 text-red-600	 "><RiCloseCircleFill /></div>
+                                                </div></div> : <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-12 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                                <div className="flex flex-col items-center justify-center">
+                                                    <svg className="w-8 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                                    </svg>
+                                                    <p className="text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> (SVG, PNG, JPG, JPEG or GIF)</p>
+                                                </div>
+                                                <input id="dropzone-file" type="file" accept=".gif,.jpg,.jpeg,.png,.svg"  {...register("profilepic")} onChange={handleimagechange} className="hidden" />
+                                            </label>}
                                     </div>
                                 </div>
                             </div>
